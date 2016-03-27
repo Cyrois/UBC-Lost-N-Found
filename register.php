2567d8f61dde198b53d2222ecc2dcd2f14230dec
cyrois <cyrois3@gmail.com>
2016-03-26 17:56:45 -0700

    added registration capability

--- register.php
+++ register.php
@@ -1,35 +1,60 @@
-<html>
-<head>
-<title>Welcome</title>
-</head>
-<body>
+
 <?php
+
 	ini_set('display_errors', 'On');
-	error_reporting(E_ALL | E_STRICT);
+    error_reporting(E_ALL | E_STRICT);
 
-	$name = $_POST["name"];
-	$password = $_POST["password"];
-        $email = $_POST["email"];
+    $email = $_POST["email"];
+    $pass = $_POST["pass"];
+    $name = $_POST["name"];
+    $phone = $_POST["phone"];
 
-	$conn = mysqli_connect("localhost", "root", "hotmail33");
+    /*
+    	// To protect MySQL injection (more detail about MySQL injection)
+$myusername = stripslashes($myusername);
+$mypassword = stripslashes($mypassword);
+$myusername = mysql_real_escape_string($myusername);
+$mypassword = mysql_real_escape_string($mypassword);
+*/
 
-	if(mysqli_connect_errno()) 
-	{
-		echo "failed to connect" . mysqli_connect_error();
+    $conn = mysqli_connect("localhost", "root", "hotmail33");
+
+    if(mysqli_connect_errno()) 
+    {
+        echo "failed to connect" . mysqli_connect_error();
+    }
+
+    mysqli_select_db($conn,"lostnfound");
+
+    $searchUser = "select * from user where email='$email'";
+
+    $result=mysqli_query($searchUser);
+
+	// Mysql_num_row is counting table row
+	$count=mysql_num_rows($result);
+
+	//check if that email is already registered
+	if($count>0){
+		$insert = "insert into user values ('$email', '$pass', '$name', '$phone')";
+
+		if (mysqli_query($conn, $insert)) {
+			header('Location: homepage.html');
+			exit;		   
+ 		} else {
+		    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+
+		/*
+		 //hash the password
+        $hashedpassword = $user->password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+        //create the activation code
+        $activasion = md5(uniqid(rand(),true));
+        */
 	}
-	echo "connected successfully!";
-
-	mysqli_select_db($conn,"test_db");
-
-	$sql = "insert into user (name, password, email)
-		values
-		('$name', '$password', '$email')";
-	if(!mysqli_query($conn, $sql)) 
-	{
-		die("error: " . mysqli_error($conn));
+	else {
+		echo "That email is already registered";
 	}
 
-	mysqli_close($conn);
+    mysqli_close($conn);
 ?>
-</body>
-</html>
